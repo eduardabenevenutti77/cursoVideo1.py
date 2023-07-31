7878da8103465b6a96bebd3b742203a10c60f2c7
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-31 17:30:28 -0300

    Update NumerosIntervalo.py

--- mundo2 - python/for/NumerosIntervalo.py
+++ mundo2 - python/for/NumerosIntervalo.py
@@ -1,1 +1,7 @@
 #crie um programa que mostre na tela todos os números pares no intervalo entre 1 e 50
+
+print('\n\t \033[1;33mVeja todo os números pares que existem no intervalo de 1 à 50\033[m\n')
+
+for c in range(0, 50):
+    if(c%2==0):
+        print(c)
