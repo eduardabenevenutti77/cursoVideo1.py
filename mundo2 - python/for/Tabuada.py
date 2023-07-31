3dce63b941ab5333df39510ad6eeecdbfcae05c1
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-31 17:25:32 -0300

    Update Tabuada.py

--- mundo2 - python/for/Tabuada.py
+++ mundo2 - python/for/Tabuada.py
@@ -1,1 +1,9 @@
 #mostre a tabuada de um número usando o laço for
+
+print('\n\t \033[1;33mVerifique a tabuada\033[m\n')
+
+n = int(input(' Informe o número que você deseja descobrir a tabuada => '))
+print('\n')
+
+for c in range(0, 11):
+    print("{} x {} = {}".format(c,n,n*c))
