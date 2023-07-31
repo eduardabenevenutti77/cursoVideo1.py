e4b64b112ac20836c8f92f3a0fb4afb3de1e3cb4
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-31 17:29:14 -0300

    Update ProgressaoAritmetica.py

--- mundo2 - python/for/ProgressaoAritmetica.py
+++ mundo2 - python/for/ProgressaoAritmetica.py
@@ -1,1 +1,12 @@
 #leia o 1º termo e a razão de uma PA, no final mostre os 10 primeiros termos dessa progressão
+
+print('\n\t \033[1;33mCalcelue uma progressão aritmética\033[m\n')
+
+Termos = int(input(' Informe o valor do 1º termo => '))
+Razao = int(input(' Informe a razão da P.A. =>'))
+t = Termos
+print(t)
+for c in range(0, 9):
+    t = t + Razao
+    print(t)
+
