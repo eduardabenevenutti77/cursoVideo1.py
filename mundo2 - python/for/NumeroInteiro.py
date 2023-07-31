bdab85fa40d6e5b9cbac16081dd4b7af32d12901
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-31 17:28:00 -0300

    Update NumeroInteiro.py

--- mundo2 - python/for/NumeroInteiro.py
+++ mundo2 - python/for/NumeroInteiro.py
@@ -1,1 +1,10 @@
 #leia um número inteiro e diga se é ou não primo
+
+print('\n\t \033[1;33mVerifique se o número é inteiro ou não\033[m\n')
+
+for c in range(0, 1):
+    num = int(input('\n Digite o valor => '))
+    if(num%num==0):
+        print(' O número {} é primo...'.format(num))
+    else:
+        print(' O número {} não é primo...'.format(num))
