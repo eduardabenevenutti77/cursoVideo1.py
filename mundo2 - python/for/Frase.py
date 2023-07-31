2fc36e9cad6f113cfd2d65ecf4534d5925ca5197
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-31 17:27:33 -0300

    Update Frase.py

--- mundo2 - python/for/Frase.py
+++ mundo2 - python/for/Frase.py
@@ -1,2 +1,14 @@
 #leia uma frase e diga se ela é um palíndromo
 #desconsidere os espaços
+
+print('\n\t \033[1;33mVerifique se a frase é um palindroma\033[m\n')
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+c = frase.split()
+frasejunta = ''.join(c)
+inverso = frasejunta[::-1]
+
+if inverso != frasejunta:
+    print('A frase: {} não é um Palíndromo'.format(frase))
+else:
+    print('A frase: {} é um Palíndromo'.format(frase))
