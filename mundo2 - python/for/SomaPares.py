cb0b7f3593dcf79a318abc727e924fb69f1013fa
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-31 17:28:30 -0300

    Update SomaPares.py

--- mundo2 - python/for/SomaPares.py
+++ mundo2 - python/for/SomaPares.py
@@ -1,1 +1,10 @@
 #leia 6 números inteiros e mostre somente os pares, se o valor foi impar desconsidere-o
+
+print('\n\t \033[1;33mCalcelue a soma dos números pares\033[m\n')
+
+for c in range(1, 6):
+    num = int(input('\n Digite {}º valor => '.format(c)))
+    if (num%2==0):
+        print(' \033[1;33mO número informado é par!!\033[m')
+    else:
+        print(' \033[1;31mO número foi desconsiderado, pois é um número ímpar..\033[m')
