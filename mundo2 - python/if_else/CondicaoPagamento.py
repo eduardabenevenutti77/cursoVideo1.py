892f4914fb07237d3851b0de6e7df6986e7d2fb5
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-07-30 21:43:09 -0300

    Update CondicaoPagamento.py

--- mundo2 - python/if_else/CondicaoPagamento.py
+++ mundo2 - python/if_else/CondicaoPagamento.py
@@ -1,5 +1,5 @@
 #calcule o valor a ser pago por um produto, considerando seu preço normal e condição de pagamento
-#à vista dinheiro/cartão: 10% de desconto
+#à vista dinheiro: 10% de desconto
 #à vista no cartão: 5% de desconto
 #em até 2x no cartão: preço normal
 #3x ou mais no cartão: 20% de juros
@@ -7,20 +7,24 @@
 print('\n\t \033[1;33mCondições de pagamento!\033[m')
 
 ValorProduto = float(input('\n Informe o valor do produto => '))
-MetodoPagamento = str(input('\n Informe o método de pagamento - cartão ou dinheiro => '))
-Parcelas = int(input('\n Informe a quantidade de parcelas => '))
+print('''\n Selecione a forma de pagamento:
+\n[1] à vista em  dinheiro
+[2] à vista no cartão 
+[3] em 2x no cartão
+[4] 3x ou mais no cartão''')
+OpcaoPagamento = str(input('\n Informe a opcão selecionada para o pagamento => '))
 
-if MetodoPagamento=='dinheiro':
+if OpcaoPagamento=='1':
     CalculoDesconto = (ValorProduto*0.1)
     DescontoAplicado = (ValorProduto-CalculoDesconto)
-    print('\n Querido(a) cliente, como o seu pagamento foi realizado à vista por {}. Será descontado 10% do valor da peça comprada, saindo por somente R$ {}!'.format(MetodoPagamento,DescontoAplicado))
-elif MetodoPagamento=='cartão':
+    print('\n Querido(a) cliente, como o seu pagamento foi realizado à vista por dinheiro. Será descontado 10% do valor da peça comprada, saindo por somente R$ {:.2f}!'.format(DescontoAplicado))
+elif OpcaoPagamento=='2':
     CalculoDesconto = (ValorProduto * 0.05)
     DescontoAplicado = (ValorProduto - CalculoDesconto)
-    print('\n Querido(a) cliente, como o seu pagamento foi realizado à vista por {}. Será descontado 5% do valor da peça comprada, saindo por somente R$ {}!'.format(MetodoPagamento, DescontoAplicado))
-elif Parcelas<=2:
-    print('\n Querido(a) cliente, como o seu pagamento foi realizado em 2x no {}, não ocorrerá desconto no valor da peça. Sendo assim, saindo por R$ {}!'.format(MetodoPagamento,ValorProduto))
-elif Parcelas>=3:
+    print('\n Querido(a) cliente, como o seu pagamento foi realizado à vista por cartão. Será descontado 5% do valor da peça comprada, saindo por somente R$ {:.2f}!'.format(DescontoAplicado))
+elif OpcaoPagamento=='3':
+    print('\n Querido(a) cliente, como o seu pagamento foi realizado em 2x no cartão, não ocorrerá desconto no valor da peça. Sendo assim, saindo por R$ {:.2f}!'.format(ValorProduto))
+elif OpcaoPagamento=='4':
     CalculoJuros = (ValorProduto * 0.2)
     JurosAplicado = (ValorProduto + CalculoJuros)
-    print('\n Querido(a) cliente, como o seu pagamento foi realizado em {}x no {}. Será cobrado um juros de 10% do valor da peça comprada, saindo por R$ {}!'.format(Parcelas, MetodoPagamento, JurosAplicado))
+    print('\n Querido(a) cliente, como o seu pagamento foi realizado em 3x no cartão. Será cobrado um juros de 10% do valor da peça comprada, saindo por R$ {:.2f}!'.format(JurosAplicado))
