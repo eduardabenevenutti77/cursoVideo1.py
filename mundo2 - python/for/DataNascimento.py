d63a68c2f455690197f3bfb123b4685e4de80418
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-08-07 20:44:22 -0300

    Update DataNascimento.py

--- mundo2 - python/for/DataNascimento.py
+++ mundo2 - python/for/DataNascimento.py
@@ -1,1 +1,11 @@
 #leia o ano de aniversário de 7 pessoas, e mostre quantas pessoas ainda não atigiram a maioridade e quantas já são adultas - 21 anos
+from datetime import date
+print('\n\t \033[1;33mVerifique quem atingiu a maioridade \033[m\n')
+anoATUAL = date.today().year
+for c in range(0,7):
+    anoNasc = int(input("\n Informe o ano do seu nascimento: "))
+    verifica = (anoATUAL - anoNasc)
+    if(verifica>=21):
+        print("\n Parabénsss, você é maior de idade!!!")
+    else:
+        print("\n Pela lei, você é considerado menor de idade :3")
