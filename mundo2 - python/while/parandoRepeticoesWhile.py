6999212be5180d6b31ec016a28d69e02ccaa0811
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-08-17 09:36:20 -0300

    Update parandoRepeticoesWhile.py

---
+++ mundo2 - python/while/parandoRepeticoesWhile.py
@@ -0,0 +1,30 @@
+'''
+utilização de break e ‘loopings’ em while
+
+break = interrompe a repetição
+
+while true: (cria loop)
+    if terra:
+        passo
+    if espaço:
+        pula
+    if moeda:
+        pega
+    if trofeu:
+        pula
+        break
+pega
+
+true = cria um loop
+'''
+
+cont = 1
+while cont <=10:
+    print(cont, ' -> ', end='')
+    cont += 1
+print("acabouuu")
+
+while True:
+    print(cont, ' -> ', end='')
+    cont += 1
+print("acabouuu")
