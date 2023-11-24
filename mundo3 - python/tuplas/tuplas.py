cdec0780a617ec853a22fe01dda2c8f38258da66
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-24 08:30:14 -0300

    update in tuplas

---
+++ mundo3 - python/tuplas/tuplas.py
@@ -0,0 +1,10 @@
+# variáveis compostas - tuplas, listas e dicionários
+#  = (atribui valor a variável - simples)
+# variável simples (recebe apenas 1 elemento) | variável composta (recebe vários valores)
+# variáveis compostas: tuplas, listas e dicionários
+# tuplas: identifica os elementos por indices
+# print(nome_tupla) - mostra todos os elementos | print(nome_tupla[indice]) - mostra somente o elemento desse indice | print(noome_tupla[-valor]) - mostra o último elemento
+# pode-se utilizar o método len (comprimento) | len(nome_tupla) - mostra o comprimento da tupla
+# estrutura de repetição dentro da tupla 
+    #for nome_variável_for in nome_tupla:
+        #print(nome_variável_for)
