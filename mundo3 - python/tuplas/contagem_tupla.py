34c65f60f29d8ef90fccd5d5f37ff2583665b7e6
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-01-05 08:28:49 -0300

    Update contagem_tupla.py

--- mundo3 - python/tuplas/contagem_tupla.py
+++ mundo3 - python/tuplas/contagem_tupla.py
@@ -1,2 +1,11 @@
 #crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso de 
 #zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 a 20) e mostrá-lo por extenso
+
+valores = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+valor_usuario = int(input('Informe um valor entre 0 a 20: '))
+
+if 0 <= valor_usuario <= 20:
+    print('O valor informado pelo o usuário é: {}'.format(valores[valor_usuario]))
+else:
+    print('O valor informado está fora dos valores permitidos! Informe outro valor ;)')
