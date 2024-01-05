11b5be7c53aef81b5bb60a7ca29e5a1b7fd8cba2
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-01-05 08:46:03 -0300

    Update futebol.py

--- mundo3 - python/tuplas/futebol.py
+++ mundo3 - python/tuplas/futebol.py
@@ -2,3 +2,17 @@
 #depois mostre:
 #a) apenas os 5 primeiros colocados; b) os últimos 4 colocados na tabela; 
 #c) uma lista com os times em ordem alfabética; d) em que posição na tabela está o tima da chapecoense.
+
+times = ('palmeiras', 'grêmio', 'atlético-mg', 'flamengo', 'botafogo', 'bragantino', 'fluminense', 'athetico-pr', 'internacional', 'fortaleza', 'são paulo', 'cuibá', 'corinthias', 'cruzeiro', 'vasco', 'santos', 'bahia', 'goiás', 'coritiba', 'américa-mg')
+
+print('''\n Selecione a opção:
+\n[1] Mostrar os 5 primeiros colocados
+[2] Mostrar os 5 últimos colocados
+[3] Listar times em ordem alfabética
+[4] Qual posição está o time da chapecoense''')
+opcao = str(input('Informe a opcão selecionada => '))
+
+if opcao == '1':
+    print('Os 5 primeiros colocados: ')
+    for equipe in range (5):
+        print(equipe[times])
