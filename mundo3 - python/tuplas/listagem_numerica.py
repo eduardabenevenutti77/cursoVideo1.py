404c4c10953403fe6da4ac96808a5083c2423086
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-01-08 12:54:11 -0300

    update

--- mundo3 - python/tuplas/listagem_numerica.py
+++ mundo3 - python/tuplas/listagem_numerica.py
@@ -5,3 +5,24 @@ import random
 
 valores = tuple(random.randint(1, 10) for _ in range(5))
 print(valores)
+
+print('''\nSelecione a opção:
+\n[1] Mostrar todos os valores
+[2] Mostrar o maior número da tupla
+[3] Mostrar o menor número da tupla
+''')
+opcao = str(input('Informe a opcão selecionada => '))
+
+if opcao == '1':
+    print('Valores: ')
+    print(valores)
+elif opcao == '2':
+    print('Maior: ')
+    maior = max(valores)
+    print(maior)
+elif opcao == '3':
+    print('Menor: ')
+    menor = min(valores)
+    print(menor)
+else:
+    print('A opção informada é inválida!')
