59088d9adca7c52ca9e7fd77b2fd5b74362153c3
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-01-08 12:47:58 -0300

    update

--- mundo3 - python/tuplas/listagem_numerica.py
+++ mundo3 - python/tuplas/listagem_numerica.py
@@ -1,2 +1,7 @@
 #crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
-#depois disso, mostre a listagem de números gerados e tabém indique o menor e o maio número da 
+#depois disso, mostre a listagem de números gerados e tabém indique o menor e o maio número da tupla
+
+import random
+
+valores = tuple(random.randint(1, 10) for _ in range(5))
+print(valores)
