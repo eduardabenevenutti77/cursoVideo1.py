a9df3a8f59eded7e55f9629d84dffb4658b8d639
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-01-09 12:02:55 -0300

    update

--- mundo3 - python/tuplas/valores_tupla.py
+++ mundo3 - python/tuplas/valores_tupla.py
@@ -2,3 +2,10 @@
 #a) quantas vezes apareceu o valor 4
 #b) em que posição foi digitado o primeiro valor 3
 #c) quais foram os números pares
+
+valor01 = int(input('Informe o 1º valor => ')
+valor02 = int(input('Informe o 2º valor => ')
+valor03 = int(input('Informe o 3º valor => ')
+valor04 = int(input('Informe o 4º valor => ')
+
+valores = (valor01, valor02, valor03, valor04)
