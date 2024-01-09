5be4156eba4f521a9f2a1fac0d9877169a5d4b08
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-01-09 12:25:50 -0300

    update

--- mundo3 - python/tuplas/palavras.py
+++ mundo3 - python/tuplas/palavras.py
@@ -1,1 +1,7 @@
 #crie uma tupla com várias palavras (não pode usar acentos). depois disso, mostre para cada palavra, quais são as suas vogais
+
+palavras = ('abacate', 'limao', 'uva', 'carro', 'caminhar', 'correr', 'lutar', 'gostar', 'gastar', 'comer', 'cuidar', 'amar', 'levar')
+
+for vogal in palavras:
+  palavra_vogal = [letra for letra in vogal if letra.lower() in 'aeiou']
+  print('As vogais em {} são: {}'.format(vogal, ', '.join(palavra_vogal)))
